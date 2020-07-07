07677b89c10d52acb12fdd38649253d2f2813745
Dennis Baldwin <dennisbaldwin@gmail.com>
2020-07-07 18:05:17 -0500

    Testing endpoints

--- tests/receiver_pwm/receiver_pwm.ino
+++ tests/receiver_pwm/receiver_pwm.ino
@@ -1,21 +1,33 @@
-byte CH2_PWM_PIN = 18; // CH2 left elevon
-byte CH3_PWM_PIN = 19; // CH3 right elevon
- 
+/*
+ * Tested in conjuction with Taranis delta mixing where endpoints are 50% of total
+ */
+
+byte CH1_PWM_PIN = 18; // CH1 throttle
+byte CH2_PWM_PIN = 19; // CH2 left elevon
+byte CH3_PWM_PIN = 20; // CH3 right elevon
+
+int ch1_pwm_value;
 int ch2_pwm_value;
 int ch3_pwm_value;
  
 void setup() {
+  pinMode(CH1_PWM_PIN, INPUT);
   pinMode(CH2_PWM_PIN, INPUT);
   pinMode(CH3_PWM_PIN, INPUT);
   Serial.begin(115200);
 }
  
 void loop() {
+
+  // Get the PWM values from the receiver
+  ch1_pwm_value = pulseIn(CH1_PWM_PIN, HIGH);
   ch2_pwm_value = pulseIn(CH2_PWM_PIN, HIGH);
   ch3_pwm_value = pulseIn(CH3_PWM_PIN, HIGH);
 
   // Print the values
+  Serial.print(ch1_pwm_value);
+  Serial.print("\t");
   Serial.print(ch2_pwm_value);
-  Serial.print("/t");
+  Serial.print("\t");
   Serial.println(ch3_pwm_value);
 }
