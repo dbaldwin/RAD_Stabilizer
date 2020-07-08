1d483877340dff79431e8d9b3c3e7db19fab6e25
Dennis Baldwin <dennisbaldwin@gmail.com>
2020-07-08 14:17:17 -0500

    Sweep between 0 and 180 and back

--- tests/servo_sweep/servo_sweep.ino
+++ tests/servo_sweep/servo_sweep.ino
@@ -1,18 +1,46 @@
 // This example sketch tests CH2 (left elevon) and CH3 (right elevon) servos
+// Pitch - positive nose up (this means both elevons towards the sky)
+// Roll - positive right wing down (this means right elevon up an left elevon down)
 
 #include "Servo.h"
 
 Servo leftElevon;
 Servo rightElevon;
+int counter = 0;
+
+int direction = 1;
 
 void setup() {
-  leftElevon.attach(9);
-  rightElevon.attach(10);
+
+  
+  leftElevon.attach(5);
+  rightElevon.attach(6);
+
+  leftElevon.write(0);
+  rightElevon.write(0);
 
 }
 
 void loop() {
 
-  leftElevon.right(0);
+  leftElevon.write(counter);
+  rightElevon.write(counter);
+
+
+  if (counter == 180) {
+    direction = 0;
+  } else if (counter == 0) {
+    direction = 1;
+  }
+
+  if (direction) {
+    counter++;
+  } else {
+    counter--;
+  }
+
+  Serial.println(counter);
+
+  delay(100);
 
 }
